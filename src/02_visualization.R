a2136fcca0e9de50d4c8e43ddb19f2c62a1b0eef
beallen <ballen@ualberta.ca>
2022-10-20 09:25:37 -0600

    functionalize the figure calls

--- src/02_visualization.R
+++ src/02_visualization.R
@@ -4,7 +4,7 @@
 # Last Updated: October 11th, 2022
 # Author: Brandon Allen
 # Objectives: Visualize the two native cover indicators
-# Keywords: Notes, Visualization
+# Keywords: Notes, Visualization, Forest recovery
 #
 
 #########
@@ -29,9 +29,19 @@ library(ggplot2)
 library(MetBrewer)
 library(sf)
 
+# Source scripts
+source("src/visualization_functions.R")
+
 # Load the results
 native.cover.2010 <- read_sf("data/processed/2010/native_cover_HFI2010.shp")
 native.cover.2018 <- read_sf("data/processed/2018/native_cover_HFI2018.shp")
+
+# Create the trend results
+terrestrial.trend <- data.frame(NC_2010 = native.cover.2010$UpCov,
+                                NC_2018 = native.cover.2018$UpCov)
+
+aquatic.trend <- data.frame(NC_2010 = native.cover.2010$LowCov,
+                            NC_2018 = native.cover.2018$LowCov)
 
 #
 # Terrestrial
@@ -42,22 +52,9 @@ png(paste0("results/figures/terrestrial-native-cover-HFI2010.png"),
     height = 2400, 
     res = 300)
 
-ggplot() + 
-        geom_sf(data = native.cover.2010, aes(fill = UpCov), show.legend = TRUE) +
-        scale_fill_gradientn(name = paste0("Native Cover (%)"), colors = met.brewer(name = "Hiroshige", n = 100, type = "continuous"), guide = "colourbar") +
-        ggtitle(paste0("Terrestrial Native Cover")) + 
-        theme_light() +
-        theme_abmi(font = "Montserrat") +
-        theme(axis.title = element_text(size=12),
-              axis.text.x = element_text(size=12),
-              axis.text.y = element_text(size=12),
-              title = element_text(size=12), 
-              legend.title = element_text(size=12),
-              legend.text = element_text(size=12),
-              legend.key.size = unit(0.5, "cm"),
-              axis.line = element_line(colour = "black"),
-              panel.border = element_rect(colour = "black", fill=NA, size=1),
-              legend.position = c(0.20, 0.15)) 
+nc_plot(data.in = native.cover.2010, 
+        habitat = "UpCov", 
+        title = "Terrestrial Native Cover")
 
 dev.off()
 
@@ -66,32 +63,21 @@ png(paste0("results/figures/terrestrial-native-cover-HFI2018.png"),
     height = 2400, 
     res = 300)
 
-ggplot() + 
-        geom_sf(data = native.cover.2018, aes(fill = UpCov), show.legend = TRUE) +
-        scale_fill_gradientn(name = paste0("Native Cover (%)"), colors = met.brewer(name = "Hiroshige", n = 100, type = "continuous"), guide = "colourbar") +
-        ggtitle(paste0("Terrestrial Native Cover")) + 
-        theme_light() +
-        theme_abmi(font = "Montserrat") +
-        theme(axis.title = element_text(size=12),
-              axis.text.x = element_text(size=12),
-              axis.text.y = element_text(size=12),
-              title = element_text(size=12), 
-              legend.title = element_text(size=12),
-              legend.text = element_text(size=12),
-              legend.key.size = unit(0.5, "cm"),
-              axis.line = element_line(colour = "black"),
-              panel.border = element_rect(colour = "black", fill=NA, size=1),
-              legend.position = c(0.20, 0.15)) 
+nc_plot(data.in = native.cover.2018, 
+        habitat = "UpCov", 
+        title = "Terrestrial Native Cover")
 
 dev.off()
-
-terrestrial.trend <- data.frame(NC_2010 = native.cover.2010$UpCov,
-                                NC_2018 = native.cover.2018$UpCov)
 
 png(paste0("results/figures/terrestrial-native-cover-trend.png"),
     width = 1800,
     height = 1800, 
     res = 300)
+
+trend_plot(data.in = terrestrial.trend, 
+           x = "2010", 
+           y = "2018", 
+           title = "Terrestrial Native Cover")
 
 ggplot(data = terrestrial.trend, aes(x = NC_2010, y = NC_2018)) + 
         geom_point() +
@@ -115,22 +101,9 @@ png(paste0("results/figures/aquatic-native-cover-HFI2010.png"),
     height = 2400, 
     res = 300)
 
-ggplot() + 
-        geom_sf(data = native.cover.2010, aes(fill = LowCov), show.legend = TRUE) +
-        scale_fill_gradientn(name = paste0("Native Cover (%)"), colors = met.brewer(name = "Hiroshige", n = 100, type = "continuous"), guide = "colourbar") +
-        ggtitle(paste0("Aquatic Native Cover")) + 
-        theme_light() +
-        theme_abmi(font = "Montserrat") +
-        theme(axis.title = element_text(size=12),
-              axis.text.x = element_text(size=12),
-              axis.text.y = element_text(size=12),
-              title = element_text(size=12), 
-              legend.title = element_text(size=12),
-              legend.text = element_text(size=12),
-              legend.key.size = unit(0.5, "cm"),
-              axis.line = element_line(colour = "black"),
-              panel.border = element_rect(colour = "black", fill=NA, size=1),
-              legend.position = c(0.20, 0.15)) 
+nc_plot(data.in = native.cover.2010, 
+        habitat = "LowCov", 
+        title = "Aquatic Native Cover")
 
 dev.off()
 
@@ -139,43 +112,21 @@ png(paste0("results/figures/aquatic-native-cover-HFI2018.png"),
     height = 2400, 
     res = 300)
 
-ggplot() + 
-        geom_sf(data = native.cover.2018, aes(fill = LowCov), show.legend = TRUE) +
-        scale_fill_gradientn(name = paste0("Native Cover (%)"), colors = met.brewer(name = "Hiroshige", n = 100, type = "continuous"), guide = "colourbar") +
-        ggtitle(paste0("Aquatic Native Cover")) + 
-        theme_light() +
-        theme_abmi(font = "Montserrat") +
-        theme(axis.title = element_text(size=12),
-              axis.text.x = element_text(size=12),
-              axis.text.y = element_text(size=12),
-              title = element_text(size=12), 
-              legend.title = element_text(size=12),
-              legend.text = element_text(size=12),
-              legend.key.size = unit(0.5, "cm"),
-              axis.line = element_line(colour = "black"),
-              panel.border = element_rect(colour = "black", fill=NA, size=1),
-              legend.position = c(0.20, 0.15)) 
+nc_plot(data.in = native.cover.2018, 
+        habitat = "LowCov", 
+        title = "Aquatic Native Cover")
 
 dev.off()
-
-aquatic.trend <- data.frame(NC_2010 = native.cover.2010$LowCov,
-                                NC_2018 = native.cover.2018$LowCov)
 
 png(paste0("results/figures/aquatic-native-cover-trend.png"),
     width = 1800,
     height = 1800, 
     res = 300)
 
-ggplot(data = aquatic.trend, aes(x = NC_2010, y = NC_2018)) + 
-        geom_point() +
-        ggtitle(paste0("Aquatic Native Cover")) + 
-        geom_abline(slope = 1) +
-        ylim(c(0,100)) +
-        xlim(c(0,100)) +
-        xlab("Native Cover 2010") +
-        ylab("Native Cover 2018") +
-        theme_light() +
-        theme_abmi(font = "Montserrat")
+trend_plot(data.in = aquatic.trend, 
+           x = "NC_2010", 
+           y = "NC_2018", 
+           title = "Aquatic Native Cover")
 
 dev.off()
 
@@ -201,6 +152,49 @@ ggplot(data = native.cover.2018, aes(x = LowCov, y = UpCov)) +
 
 dev.off()
 
+###################
+# Forest Recovery # 
+###################~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # Clear memory
 rm(list=ls())
 gc()
+
+# Load libraries
+library(abmi.themes)
+library(ggplot2)
+library(MetBrewer)
+
+# Source scripts
+source("src/visualization_functions.R")
+
+# Load data
+load("data/lookup/harvest-recovery.Rdata")
+
+# Standardize the data
+harvest.recovery <- data.frame(Stand = c(rep("Deciduous", 101), 
+                                         rep("Coniferous", 101)),
+                               Age = c(harvest.recovery$Age, 
+                                       harvest.recovery$Age),
+                               Recovery = c(harvest.recovery$Deciduous,
+                                            harvest.recovery$Coniferous))
+
+# Visualize
+png(paste0("results/figures/forest-recovery-curves.png"),
+    width = 2400,
+    height = 1600, 
+    res = 300)
+
+ggplot(data = harvest.recovery, aes(x = Age, y = Recovery, fill = Stand, col = Stand)) + 
+        geom_point() +
+        scale_color_manual(values = met.brewer(name = "Egypt", n = 2, type = "discrete")) +
+        scale_fill_manual(values = met.brewer(name = "Egypt", n = 2, type = "discrete")) +
+        ggtitle(paste0("Forest Recovery Curves")) + 
+        xlab("Age") +
+        ylab("Stand Recovery (%)") +
+        ylim(c(0,100)) +
+        xlim(c(0,100)) +
+        theme_light() +
+        theme_abmi(font = "Montserrat")
+
+dev.off()
